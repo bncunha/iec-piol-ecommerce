e7a57444f1cd9286fb60fe003163a09cf3254e3c
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2022-08-21 15:13:49 -0300

    Login page e cadastro page

--- iec-front/src/app/app-routing.module.ts
+++ iec-front/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  {path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)},
+  {path: 'cadastro', loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule)},
   {path: 'produto', loadChildren: () => import('./pages/product/product.module').then(m => m.ProductModule)},
   {path: 'carrinho', loadChildren: () => import('./pages/cart/cart.module').then(m => m.CartModule)},
   {path: 'finalizar', loadChildren: () => import('./pages/checkout/checkout.module').then(m => m.CheckoutModule)},
